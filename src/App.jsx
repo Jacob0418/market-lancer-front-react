56595d70bd2b9a1ea0c2083005ae23f103483a26
rafa133169 <rafaelmonje1331@gmail.com>
2024-11-08 20:07:35 -0500

    Config Routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,22 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/dev" element={<Home />} />
+        {/* Inicio New Vendedor */}
+          <Route path="/seller_onboarding/overview" element={<Home/>} />
+          <Route path="/seller_onboarding/overview/do" element={<Home/>} />
+          <Route path="/seller_onboarding/overview/dont" element={<Home/>} />
+          <Route path="/seller_onboarding/personal_info" element={<Home/>} />
+          <Route path="/seller_onboarding/professional_info" element={<Home/>} />
+          <Route path="/seller_onboarding/account_security" element={<Home/>} />
+        {/* Fin New Vendedor */}
+        {/* Inicio New Servicio */}
+        <Route path="/seller_onboarding/overview" element={<Home/>} />
+          <Route path="/seller_onboarding/overview/do" element={<Home/>} />
+          <Route path="/seller_onboarding/overview/dont" element={<Home/>} />
+          <Route path="/seller_onboarding/personal_info" element={<Home/>} />
+          <Route path="/seller_onboarding/professional_info" element={<Home/>} />
+          <Route path="/seller_onboarding/account_security" element={<Home/>} />
+        {/* Fin New Servicio */}
       </Routes>
     </BrowserRouter>
   )
